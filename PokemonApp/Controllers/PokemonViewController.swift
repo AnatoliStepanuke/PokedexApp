bb900b8fdcf78e228e52269cb911fba1a4068301
Anatoli Stepanuke <anatolistepanuke@yandex.by>
2022-11-10 15:08:14 +0300

    Initial pokemon-list feature branch Added UITableViewDelegate and UITableViewDataSource in PokemonViewController

--- PokemonApp/Controllers/PokemonViewController.swift
+++ PokemonApp/Controllers/PokemonViewController.swift
@@ -1,12 +1,25 @@
 import UIKit
 
-final class PokemonViewController: UIViewController {
+final class PokemonViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     // MARK: - Constants
     // MARK: - Properties
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        setupView()
     }
     // MARK: - Setups
+    private func setupView() {
+        view.backgroundColor = .blue
+    }
+
+    // MARK: - UITableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
 }
