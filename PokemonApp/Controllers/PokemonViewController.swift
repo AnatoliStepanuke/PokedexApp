98609224e967e02e11866c4e700110974dc9b17f
Anatoli Stepanuke <anatolistepanuke@yandex.by>
2022-11-10 15:33:12 +0300

    Added tableView in PokemonViewController

--- PokemonApp/Controllers/PokemonViewController.swift
+++ PokemonApp/Controllers/PokemonViewController.swift
@@ -2,15 +2,27 @@ import UIKit
 
 final class PokemonViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     // MARK: - Constants
+    private let tableView = UITableView()
+
     // MARK: - Properties
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
+        setupTableView()
     }
     // MARK: - Setups
     private func setupView() {
-        view.backgroundColor = .blue
+        view.addSubview(tableView)
+    }
+
+    private func setupTableView() {
+        tableView.fillEntireView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = 120
+        tableView.backgroundColor = .blue
+        tableView.separatorStyle = .none
     }
 
     // MARK: - UITableView
