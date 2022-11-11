6331937dd4d08987a41cbb1a2319bcb1d7519065
Anatoli Stepanuke <anatolistepanuke@yandex.by>
2022-11-11 21:34:04 +0300

    PokemonCell code refactoring

--- PokemonApp/Manager/APIManager.swift
+++ PokemonApp/Manager/APIManager.swift
@@ -1,14 +1,9 @@
 import Alamofire
 
 final class APIManager {
-    // MARK: - Enum
-    // MARK: Private
     private enum Constants {
         static let baseURL: String = "https://pokeapi.co/api/v2/pokemon"
     }
-
-    // MARK: - Type constant
-    // MARK: Public
     static let instance = APIManager()
 
     // MARK: - Init
@@ -18,9 +13,7 @@ final class APIManager {
     static func getAllPokemons(completion: @escaping((Pokemon) -> Void)) {
         AF.request(Constants.baseURL).responseDecodable(of: Pokemon.self) { response in
             switch response.result {
-            case .success(let data):
-                print(data)
-                completion(data)
+            case .success(let data): completion(data)
             case .failure(let error): print(error)
             }
         }
