55d9cf9410aa38a53eebc85a911e4a59c858f68d
Anatoli Stepanuke <anatolistepanuke@yandex.by>
2022-11-14 19:30:28 +0300

    APIManager code refactoring

--- PokemonApp/Extensions/APIManager/APIManager+constants.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-extension APIManager {
-    enum Constants {
-        static var baseURL: String = "https://pokeapi.co/api/v2/pokemon"
-    }
-}
--- PokemonApp/Manager/APIManager.swift
+++ PokemonApp/Manager/APIManager.swift
@@ -1,19 +1,21 @@
 import Alamofire
 
 final class APIManager {
-    static let instance = APIManager()
+    // MARK: - Properties
+    var baseURL: String = "https://pokeapi.co/api/v2/pokemon"
 
     // MARK: - Init
+    static let instance = APIManager()
     private init() { }
 
     // MARK: - API
     func getPokemonsNextPage(completion: @escaping((Result) -> Void)) {
-        AF.request(Constants.baseURL).responseDecodable(of: Result.self) { response in
+        AF.request(baseURL).responseDecodable(of: Result.self) { response in
             switch response.result {
             case .success(let data):
                 print(response)
                 completion(data)
-                Constants.baseURL = data.nextPage
+                self.baseURL = data.nextPage
             case .failure(let error): print(error)
             }
         }
