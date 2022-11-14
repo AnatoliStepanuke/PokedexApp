42f9c19ff11637ea9f14349cdb646e542fa7ac25
Anatoli Stepanuke <anatolistepanuke@yandex.by>
2022-11-15 00:25:05 +0300

    Added TypeElement and Species element in PokemonDetails

--- PokemonApp/Models/PokemonDetails.swift
+++ PokemonApp/Models/PokemonDetails.swift
@@ -4,12 +4,24 @@ import Foundation
 struct PokemonDetails: Codable {
     let name: String
     let sprites: Sprites
+    let types: [TypeElement]
 }
 
+// MARK: - Sprites
 struct Sprites: Codable {
     let frontDefault: String?
 
     enum CodingKeys: String, CodingKey {
         case frontDefault = "front_default"
     }
+}
+
+// MARK: - Types
+struct TypeElement: Codable {
+    let type: Species
+}
+
+// MARK: - Species
+struct Species: Codable {
+    let name: String
 }
